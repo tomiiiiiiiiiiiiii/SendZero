12ada19557ca52e556faccd701fe79591e676338
t0mi <41620063+tomiiiiiiiiiiiiii@users.noreply.github.com>
2026-09-25 13:42:19 +0200

    Add master storage-node allocator

---
+++ api/allocate.php
@@ -0,0 +1,59 @@
+<?php
+require_once __DIR__ . '/common.php';
+
+sz_require_role('master');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    sz_json(array('ok' => false, 'error' => 'method_not_allowed'), 405);
+}
+
+$fileSize = (float)sz_read_request_value('file_size', 0);
+$ttl = (int)sz_read_request_value('ttl', 86400);
+$once = sz_read_request_value('once', '0') === '1';
+
+if ($fileSize <= 0 || $fileSize > MAX_FILE_BYTES) {
+    sz_json(array('ok' => false, 'error' => 'invalid_file_size'), 413);
+}
+
+if (!in_array($ttl, $GLOBALS['SENDZERO_TTLS'], true)) {
+    sz_json(array('ok' => false, 'error' => 'invalid_ttl'), 400);
+}
+
+$selected = sz_master_choose_node($fileSize);
+if ($selected === false) {
+    sz_json(array('ok' => false, 'error' => 'no_storage_node_available'), 503);
+}
+
+$nodeId = $selected['id'];
+$node = $selected['node'];
+$secret = sz_master_node_secret($node);
+
+if ($secret === '') {
+    sz_json(array('ok' => false, 'error' => 'node_not_configured'), 503);
+}
+
+try {
+    $nonce = sz_random_hex(16);
+} catch (Exception $e) {
+    sz_json(array('ok' => false, 'error' => 'server_random_unavailable'), 500);
+}
+
+$payload = array(
+    'v' => 1,
+    'node' => $nodeId,
+    'size' => (int)$fileSize,
+    'ttl' => $ttl,
+    'once' => $once ? 1 : 0,
+    'exp' => time() + SENDZERO_ALLOCATION_TTL,
+    'nonce' => $nonce
+);
+
+$token = sz_allocation_sign($payload, $secret);
+
+sz_json(array(
+    'ok' => true,
+    'node_id' => $nodeId,
+    'api_base' => sz_master_node_url($node),
+    'allocation_token' => $token,
+    'allocation_expires_at' => (int)$payload['exp']
+), 200);
