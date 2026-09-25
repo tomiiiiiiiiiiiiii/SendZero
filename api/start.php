a7035a2859da66ba164be2d2a9f106f0e57c2bea
t0mi <41620063+tomiiiiiiiiiiiiii@users.noreply.github.com>
2026-09-25 13:12:53 +0200

    Allow resumable one-time download sessions

--- api/start.php
+++ api/start.php
@@ -6,6 +6,8 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 }
 
 $id = strtolower((string)sz_read_request_value('id', ''));
+$resumeToken = strtolower((string)sz_read_request_value('token', ''));
+
 if (!sz_valid_id($id)) {
     sz_json(array('ok' => false, 'error' => 'invalid_id'), 400);
 }
@@ -45,10 +47,41 @@ if (!isset($meta['state']) || $meta['state'] !== 'ready') {
 if (empty($meta['once'])) {
     flock($fh, LOCK_UN);
     fclose($fh);
-    sz_json(array('ok' => true, 'token' => ''), 200);
+    sz_json(array('ok' => true, 'token' => '', 'resumed' => false), 200);
 }
 
-if (isset($meta['download_session_expires']) && (int)$meta['download_session_expires'] > time()) {
+/*
+ * A resumable one-time download may present the token previously issued to
+ * this browser. Matching the stored token hash is enough to reclaim the
+ * session, even if the short session lease expired in the meantime, provided
+ * the transfer itself has not expired and another client has not claimed it.
+ */
+if (
+    sz_valid_token($resumeToken) &&
+    isset($meta['download_token_hash']) &&
+    sz_safe_equals($meta['download_token_hash'], hash('sha256', $resumeToken))
+) {
+    $meta['download_session_expires'] = time() + DOWNLOAD_SESSION_TTL;
+
+    ftruncate($fh, 0);
+    rewind($fh);
+    fwrite($fh, json_encode($meta));
+    fflush($fh);
+    flock($fh, LOCK_UN);
+    fclose($fh);
+    @chmod($metaPath, 0600);
+
+    sz_json(array(
+        'ok' => true,
+        'token' => $resumeToken,
+        'resumed' => true
+    ), 200);
+}
+
+if (
+    isset($meta['download_session_expires']) &&
+    (int)$meta['download_session_expires'] > time()
+) {
     flock($fh, LOCK_UN);
     fclose($fh);
     sz_json(array('ok' => false, 'error' => 'one_time_in_progress'), 409);
@@ -73,4 +106,8 @@ flock($fh, LOCK_UN);
 fclose($fh);
 @chmod($metaPath, 0600);
 
-sz_json(array('ok' => true, 'token' => $token), 200);
+sz_json(array(
+    'ok' => true,
+    'token' => $token,
+    'resumed' => false
+), 200);
